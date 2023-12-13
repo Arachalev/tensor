a73ef6fc852df2cd4768341d9e5bb1a4ceac13e0
Arachalev <ekongedet01@gmail.com>
2023-12-13 10:59:02 +0100

    dev branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center text-white justify-between p-24">
-      holla my name is ozed
+     Dev branch
     </main>
   );
 }
