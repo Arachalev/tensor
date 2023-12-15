ead445e08058f94abc5625d245840cf259aad576
Arachalev <ekongedet01@gmail.com>
2023-12-15 18:18:27 +0100

    testing horizontal scroll

---
+++ src/components/Banner/Banner.tsx
@@ -0,0 +1,32 @@
+const Banner = ({ images, speed = 5000 }:{images:[], speed:number}) => {
+    return (
+      <div className="inner">
+        <div className="wrapper">
+          <section style={{ "--speed": `${speed}ms` }}>
+            {images.map(({ id, image }) => (
+              <div className="image" key={id}>
+                <img src={image} alt={id} />
+              </div>
+            ))}
+          </section>
+          <section style={{ "--speed": `${speed}ms` }}>
+            {images.map(({ id, image }) => (
+              <div className="image" key={id}>
+                <img src={image} alt={id} />
+              </div>
+            ))}
+          </section>
+          <section style={{ "--speed": `${speed}ms` }}>
+            {images.map(({ id, image }) => (
+              <div className="image" key={id}>
+                <img src={image} alt={id} />
+              </div>
+            ))}
+          </section>
+        </div>
+      </div>
+    );
+  };
+  
+  export { Banner };
+  
