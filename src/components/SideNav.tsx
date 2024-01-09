5d3a48acb61828003cd4c5ab4a55508438e5e6e4
Arachalev <ekongedet01@gmail.com>
2024-01-09 01:46:12 +0100

    added hover states to side nav

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -42,7 +42,7 @@ const SideNav = ({ showInvestor = true }: { showInvestor?: boolean }) => {
       >
         <Link
           href=""
-          className={`font-semibold text-[13px] ${
+          className={`font-semibold text-[13px] hover:text-white ${
             variant === "dark" ? "text-darkGreen" : "text-[#74DDD0]"
           }  ml-4 `}
         >
@@ -63,12 +63,21 @@ const SideNav = ({ showInvestor = true }: { showInvestor?: boolean }) => {
               src={variant === "dark" ? arrow : arrowWhite}
               alt="arrow"
             />
-            <h4 onClick={() => router.push("/how-it-works")}>What We Do</h4>
+            <h4
+              className="hover:text-[#74DDD0]"
+              onClick={() => router.push("/how-it-works")}
+            >
+              What We Do
+            </h4>
           </div>
           {showNav.whatWedDo && (
             <div className="ml-4 flex flex-col gap-1 mt-1  text-[10px]">
-              <Link href="/how-it-works">Our Product</Link>
-              <Link href="/pricing-model">Pricing</Link>
+              <Link className={`hover:text-white`} href="/how-it-works">
+                Our Product
+              </Link>
+              <Link className={`hover:text-white`} href="/pricing-model">
+                Pricing
+              </Link>
             </div>
           )}
         </div>
@@ -88,16 +97,42 @@ const SideNav = ({ showInvestor = true }: { showInvestor?: boolean }) => {
               alt="arrow"
             />
 
-            <h4 onClick={() => router.push("/who-we-are")}>Who We Are</h4>
+            <h4
+              className="hover:text-[#74DDD0]"
+              onClick={() => router.push("/who-we-are")}
+            >
+              Who We Are
+            </h4>
           </div>
           {showNav.whoWeAre && (
             <div className="ml-4 flex flex-col gap-1 mt-1  text-[10px]">
-              <Link href="/who-we-are#leadership">Leadership</Link>
-              <Link href="/who-we-are#founder">Founder</Link>
-              <Link href="/who-we-are#our-people">Our People</Link>
-              <Link href="/who-we-are#diversity">Diversity</Link>
-              <Link href="/who-we-are#principles">Core Principles</Link>
-              <Link href="/who-we-are#community">Community</Link>
+              <Link
+                className={`hover:text-white`}
+                href="/who-we-are#leadership"
+              >
+                Leadership
+              </Link>
+              <Link className={`hover:text-white`} href="/who-we-are#founder">
+                Founder
+              </Link>
+              <Link
+                className={`hover:text-white`}
+                href="/who-we-are#our-people"
+              >
+                Our People
+              </Link>
+              <Link className={`hover:text-white`} href="/who-we-are#diversity">
+                Diversity
+              </Link>
+              <Link
+                className={`hover:text-white`}
+                href="/who-we-are#principles"
+              >
+                Core Principles
+              </Link>
+              <Link className={`hover:text-white`} href="/who-we-are#community">
+                Community
+              </Link>
             </div>
           )}
         </div>
@@ -117,25 +152,51 @@ const SideNav = ({ showInvestor = true }: { showInvestor?: boolean }) => {
               alt="arrow"
             />
 
-            <h4 onClick={() => router.push("/how-to-join")}>How to Join</h4>
+            <h4
+              className="hover:text-[#74DDD0]"
+              onClick={() => router.push("/how-to-join")}
+            >
+              How to Join
+            </h4>
           </div>
           {showNav.join && (
             <div className="ml-4 flex flex-col gap-1 mt-1  text-[10px]">
-              <Link href="/how-to-join#career">Career Development</Link>
-              <Link href="/how-to-join#path">Choose Your Path</Link>
-              <Link href="/how-to-join#interview">Interviewing</Link>
-              <Link href="/how-to-join#internships">internships</Link>
-              <Link href="/how-to-join#fellowship">Fellowship Program</Link>
-              <Link href="/how-to-join#faq">Application FAQ</Link>
+              <Link className={`hover:text-white`} href="/how-to-join#career">
+                Career Development
+              </Link>
+              <Link className={`hover:text-white`} href="/how-to-join#path">
+                Choose Your Path
+              </Link>
+              <Link
+                className={`hover:text-white`}
+                href="/how-to-join#interview"
+              >
+                Interviewing
+              </Link>
+              <Link
+                className={`hover:text-white`}
+                href="/how-to-join#internships"
+              >
+                internships
+              </Link>
+              <Link
+                className={`hover:text-white`}
+                href="/how-to-join#fellowship"
+              >
+                Fellowship Program
+              </Link>
+              <Link className={`hover:text-white`} href="/how-to-join#faq">
+                Application FAQ
+              </Link>
             </div>
           )}
         </div>
         <button
           type="button"
-          className={`  text-darkGreen ${
+          className={`  text-darkGreen hover:text-[#74DDD0]  ${
             variant === "dark"
               ? "bg-inherit border-darkGreen border-[2px] "
-              : "bg-[#E2FFFB]"
+              : "bg-[#E2FFFB] hover:bg-inherit border-darkGreen border-[2px] "
           }    w-[94px]  h-7 rounded-[3px]  
           font-medium text-[9px]  whitespace-nowrap`}
         >
@@ -147,9 +208,15 @@ const SideNav = ({ showInvestor = true }: { showInvestor?: boolean }) => {
             variant === "dark" ? "text-darkGreen/70" : "text-white"
           } font-semibold  text-[13px] ml-4 flex flex-col gap-1`}
         >
-          <Link href="/faq-library">FAQ</Link>
-          <Link href="/faq-library">Library</Link>
-          <Link href="/pricing-model">Contact</Link>
+          <Link className={`hover:text-[#74DDD0]`} href="/faq-library">
+            FAQ
+          </Link>
+          <Link className={`hover:text-[#74DDD0]`} href="/faq-library">
+            Library
+          </Link>
+          <Link className={`hover:text-[#74DDD0]`} href="/pricing-model">
+            Contact
+          </Link>
         </div>
       </div>
     </div>
